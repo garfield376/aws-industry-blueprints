e0b659017472067737c44a689eece28be5f0eae9
Fei Lang <langfei@amazon.com>
2023-03-23 23:55:57 -0700

    Rename class to proper name.

--- source/modules/data_ingestion/aws-stream-ingestion-kinesis-lambda-dynamodb/lib/aws-stream-ingestion-kinesis-lambda-dynamodb-stack.ts
+++ source/modules/data_ingestion/aws-stream-ingestion-kinesis-lambda-dynamodb/lib/aws-stream-ingestion-kinesis-lambda-dynamodb-stack.ts
@@ -1,16 +1,66 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import { KinesisStreamsToLambda } from "@aws-solutions-constructs/aws-kinesisstreams-lambda";
+
+import * as ddb from "aws-cdk-lib/aws-dynamodb";
+import { LambdaToDynamoDB } from "@aws-solutions-constructs/aws-lambda-dynamodb";
+import { Construct } from "constructs";
 
 export class AwsStreamIngestionKinesisLambdaDynamodbStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const kinesisLambda = new KinesisStreamsToLambda(
+      this,
+      "KinesisLambdaConstruct",
+      {
+        lambdaFunctionProps: {
+          // Where the CDK can find the lambda function code
+          runtime: lambda.Runtime.NODEJS_16_X,
+          handler: "lambdaFunction.handler",
+          code: lambda.Code.fromAsset("lambda"),
+        }
+      }
+    );
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsStreamIngestionKinesisLambdaDynamodbQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Next Solutions Construct goes here
+    // Define the primary key for the new DynamoDB table
+    const primaryKeyAttribute: ddb.Attribute = {
+      name: "partitionKey",
+      type: ddb.AttributeType.STRING,
+    };
+
+    // Define the sort key for the new DynamoDB table
+    const sortKeyAttribute: ddb.Attribute = {
+      name: "timestamp",
+      type: ddb.AttributeType.STRING,
+    };
+
+    const lambdaDynamoDB = new LambdaToDynamoDB(
+      this,
+      "LambdaDynamodbConstruct",
+      {
+        // Tell construct to use the Lambda function in
+        // the first construct rather than deploy a new one
+        existingLambdaObj: kinesisLambda.lambdaFunction,
+        tablePermissions: "Write",
+        dynamoTableProps: {
+          partitionKey: primaryKeyAttribute,
+          sortKey: sortKeyAttribute,
+          billingMode: ddb.BillingMode.PROVISIONED,
+          removalPolicy: cdk.RemovalPolicy.DESTROY
+        },
+      }
+    );
+
+    // Add autoscaling
+    const readScaling = lambdaDynamoDB.dynamoTable.autoScaleReadCapacity({
+      minCapacity: 1,
+      maxCapacity: 50,
+    });
+
+    readScaling.scaleOnUtilization({
+      targetUtilizationPercent: 50,
+    });
   }
 }
